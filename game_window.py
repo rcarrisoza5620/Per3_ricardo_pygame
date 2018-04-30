ed51dc3a1c4c6d5dcccda7954be8c08f911dac54
Ricardo Carrisoza <RCarrisoza5620@student.pps.net>
2018-04-30 12:58:41 -0700

    Display a window with some other interesting background color

--- game_window.py
+++ game_window.py
@@ -1,15 +1,16 @@
-import pygame, sys
+import pygame, sys, pygame.color
 
 pygame.init()
 win_width = int(input("Set the width of the window. "))
 win_height = int(input("Set the height of the window. "))
-
+bg_color = pygame.color.Color("#D7E894")
 screen = pygame.display.set_mode((win_width,win_height))
 
 while True:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
 		sys.exit()
-	screen.fill((255,255,255))
+	screen.fill(bg_color)
 	pygame.display.flip()
+	
 	
