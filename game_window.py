afb50d221585b85cc2f8002b8c2ff24406154017
Ricardo Carrisoza <RCarrisoza5620@student.pps.net>
2018-04-30 12:52:42 -0700

    Rewrite the event loop so that it no longer needs a running flag

--- game_window.py
+++ game_window.py
@@ -1,14 +1,14 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 win_width = int(input("Set the width of the window. "))
 win_height = int(input("Set the height of the window. "))
 
 screen = pygame.display.set_mode((win_width,win_height))
-running = True
-while running:
+
+while True:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
-		running = False
+		sys.exit()
 		
 	
