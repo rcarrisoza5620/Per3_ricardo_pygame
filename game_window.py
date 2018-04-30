36cd0eea2ab616c2f7e09e313cc714ca8aecf10e
Ricardo Carrisoza <RCarrisoza5620@student.pps.net>
2018-04-30 12:51:16 -0700

    Ask the user for width and height inputs, then use those values to set the size of the window

--- game_window.py
+++ game_window.py
@@ -1,9 +1,14 @@
 import pygame
 
-screen = pygame.display.set_mode((640,480))
+pygame.init()
+win_width = int(input("Set the width of the window. "))
+win_height = int(input("Set the height of the window. "))
+
+screen = pygame.display.set_mode((win_width,win_height))
 running = True
 while running:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
 		running = False
+		
 	
